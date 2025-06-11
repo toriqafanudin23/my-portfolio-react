544693fa70a9da62218c700e1da099ef2aaebacf
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-11 11:27:54 +0700

    little repair

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="flex items-center justify-center bg-slate-800 py-10 inter-300 text-white">
+    <div className="flex items-center justify-center bg-slate-800 py-10 inter-300 text-white text-center">
       <p>&copy; 2025 Freelance React Developer. All rights reserved. Avan!</p>
     </div>
   );
