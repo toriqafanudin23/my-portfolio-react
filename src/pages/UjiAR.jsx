7bb886fd17131107c08a66f18f974c381bad569f
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-25 21:21:33 +0700

    little repair

--- src/pages/UjiAR.jsx
+++ src/pages/UjiAR.jsx
@@ -1,31 +1,32 @@
 const UjiCobaAR = () => {
   return (
-    <div>
-      <a-scene
-        vr-mode-ui="enabled: false"
-        embedded
-        arjs="sourceType: webcam; debugUIEnabled: false;"
-      >
-        {/* Kamera + Marker */}
-        {/* <a-marker preset="hiro"> */}
-        <a-marker
-          type="pattern"
-          url="https://bxehantrfbyenzvyrfoc.supabase.co/storage/v1/object/public/files//marker.patt"
+    <div className="flex items-center justify-center py-12 bg-black">
+      <div className="w-full max-w-xl aspect-square relative">
+        <a-scene
+          vr-mode-ui="enabled: false"
+          embedded
+          arjs="sourceType: webcam; debugUIEnabled: false;"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
         >
-          {/* 3D Box muncul di atas marker */}
-          <a-box
-            position="0 0.5 0"
-            material="color: red;"
-            animation="property: rotation; to: 0 360 0; loop: true; dur: 2000"
-          ></a-box>
-        </a-marker>
-
-        <a-entity camera></a-entity>
-      </a-scene>
-
-      {/* A-Frame dan AR.js Script */}
-      <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js@3.4.2/aframe/build/aframe-ar.min.js"></script>
+          <a-marker
+            type="pattern"
+            url="https://bxehantrfbyenzvyrfoc.supabase.co/storage/v1/object/public/files/marker.patt"
+          >
+            <a-box
+              position="0 0.5 0"
+              material="color: red;"
+              animation="property: rotation; to: 0 360 0; loop: true; dur: 2000"
+            ></a-box>
+          </a-marker>
+          <a-entity camera></a-entity>
+        </a-scene>
+      </div>
     </div>
   );
 };
