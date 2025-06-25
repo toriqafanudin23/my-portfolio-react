da653d3bf245bfc467b8743a5f304517cf7c4bc5
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-25 23:31:49 +0700

    add AR

--- src/pages/UjiAR.jsx
+++ src/pages/UjiAR.jsx
@@ -1,32 +1,14 @@
 const UjiCobaAR = () => {
   return (
-    <div className="flex items-center justify-center py-12 bg-black">
-      <div className="w-full max-w-xl aspect-square relative">
-        <a-scene
-          vr-mode-ui="enabled: false"
-          embedded
-          arjs="sourceType: webcam; debugUIEnabled: false;"
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <a-marker
-            type="pattern"
-            url="https://bxehantrfbyenzvyrfoc.supabase.co/storage/v1/object/public/files/marker.patt"
-          >
-            <a-box
-              position="0 0.5 0"
-              material="color: red;"
-              animation="property: rotation; to: 0 360 0; loop: true; dur: 2000"
-            ></a-box>
-          </a-marker>
-          <a-entity camera></a-entity>
-        </a-scene>
-      </div>
+    <div className="min-h-screen flex items-center justify-center px-4 pt-16">
+      <iframe
+        src="https://mywebar.com/p/Project_1_w7y625xuc5"
+        frameborder="0"
+        scrolling="yes"
+        seamless="seamless"
+        style="display:block; width:100%; height:100vh;"
+        allow="camera;gyroscope;accelerometer;magnetometer;xr-spatial-tracking;microphone;"
+      ></iframe>
     </div>
   );
 };
