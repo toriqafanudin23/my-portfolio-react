457de674fb856fb8ede95d7a8b4cbc43842b4e45
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-25 23:35:28 +0700

    add AR

--- src/pages/UjiAR.jsx
+++ src/pages/UjiAR.jsx
@@ -3,11 +3,9 @@ const UjiCobaAR = () => {
     <div className="min-h-screen flex items-center justify-center px-4 pt-16">
       <iframe
         src="https://mywebar.com/p/Project_1_w7y625xuc5"
-        frameborder="0"
-        scrolling="yes"
-        seamless="seamless"
-        style="display:block; width:100%; height:100vh;"
-        allow="camera;gyroscope;accelerometer;magnetometer;xr-spatial-tracking;microphone;"
+        className="w-full h-screen border-none block"
+        allow="camera; gyroscope; accelerometer; magnetometer; xr-spatial-tracking; microphone"
+        title="WebAR"
       ></iframe>
     </div>
   );
